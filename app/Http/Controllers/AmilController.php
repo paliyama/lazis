7c2e4268655fa69af98ecb99e3ec3018ef73e21d
ken <164145988+paliyama@users.noreply.github.com>
2024-03-26 16:15:40 +0700

    Add files via upload

---
+++ app/Http/Controllers/AmilController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Amil;
+use App\Http\Requests\StoreAmilRequest;
+use App\Http\Requests\UpdateAmilRequest;
+
+class AmilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAmilRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Amil $amil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Amil $amil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAmilRequest $request, Amil $amil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Amil $amil)
+    {
+        //
+    }
+}
